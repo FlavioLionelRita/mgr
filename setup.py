95cfa2a4e593ba0d41c9b336145b199bcfb9d5e0
FlavioLionelRita <flaviolrital@hotmail.com>
2021-02-27 23:10:37 +0100

    .

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'py-mgr',
   packages = ['py-mgr'],
-  version = '0.0.1',
+  version = '0.0.3',
   description = 'administration of modules and plugins ',
   author = 'Flavio Lionel Rita',
   author_email = 'flaviolrita@hotmail.com',
   url = 'https://github.com/FlavioLionelRita/py-mgr', # use the URL to the github repo
-  download_url = 'https://github.com/FlavioLionelRita/py-mgr/tarball/0.0.1',
+  download_url = 'https://github.com/FlavioLionelRita/py-mgr/tarball/0.0.3',
   keywords = ['manager', 'plugin'],
   classifiers = [],
 )
